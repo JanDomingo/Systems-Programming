43572d14c8b4a4c60d082e99e15a94e7dabfb5ce
Jan Domingo <janaarondomingo@gmail.com>
2019-03-08 13:26:33 -0800

    Created xed.c and added comments

---
+++ a2/xed.c
@@ -0,0 +1,20 @@
+/*************************************************************
+ Name: Andrew Bostros, Anthony Huynh, Jan Domingo 
+ Class Account: TODO: FILL LATER
+ Class Information: CS570, Spring 2019, Section 1
+ Project: CS530 Assignment 2
+ Assignment Information: Assignment #2, XE Dissassembler
+ File: xed.c - Source Code
+ Notes: TODO: FILL LATER
+ Date Due: April 22, 2019, 2019
+ Assignment Instructions: TODO: FILL LATER
+**************************************************************/
+
+//TODO: STEP 1: Create OPTAB and SYMTAB
+//TODO: STEP 2: Output 
+//TODO: STEP 3: 
+//TODO: STEP 3.a: Determine record type
+//TODO: STEP 3.b: Format Specifier 
+//TODO: STEP 3.c: Format 1, Format 2, Format 3,4
+//TODO: STEP 3.d: Take output of functions above. Print to source
+
