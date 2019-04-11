7f5d14f3432e730dd86a72a1dd027c75dfb956c1
Jan Domingo <janaarondomingo@gmail.com>
2019-04-11 12:52:29 -0700

    Assignment 2 files downloaded, xed folder recreated

---
+++ a2/xed/cmake-build-debug/CMakeFiles/xed.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/xed.dir/main.c.o"
+  "xed.pdb"
+  "xed"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/xed.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
