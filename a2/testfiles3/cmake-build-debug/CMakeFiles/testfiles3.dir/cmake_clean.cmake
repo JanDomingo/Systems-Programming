30644ee91425d51c811cd6e2f2a9fe1afe3a6cb5
Jan Domingo <janaarondomingo@gmail.com>
2019-04-19 13:06:46 -0700

    testfiles3 added to try creating a function for the symtab

---
+++ a2/testfiles3/cmake-build-debug/CMakeFiles/testfiles3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testfiles3.dir/main.c.o"
+  "testfiles3.pdb"
+  "testfiles3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/testfiles3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
